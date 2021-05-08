59f1080ca9bb7bf01b3bdf3ea617bd5d8ca0b531
fabiankersting <58251698+fabiankersting@users.noreply.github.com>
2021-05-08 20:26:04 +0200

    Added Crosshair, show names of interactable object + more interactable objects

--- Assets/Scripts/InteractScript.cs
+++ Assets/Scripts/InteractScript.cs
@@ -3,14 +3,38 @@ using UnityEngine;
 public class InteractScript : MonoBehaviour
 {
     [SerializeField] private float interactDistance = 5f;
+    private GameObject lastHitGO = null;
 
     void Update()
     {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+            {
+                hit.collider.transform.parent.GetComponent<DisplayText>().ChangeTextState(true);
+
+                if(lastHitGO != null && lastHitGO != hit.collider.gameObject)
+                    lastHitGO.transform.parent.GetComponent<DisplayText>().ChangeTextState(false);
+
+                lastHitGO = hit.collider.gameObject;
+            }
+            else
+            {
+                if (lastHitGO != null)
+                    lastHitGO.transform.parent.GetComponent<DisplayText>().ChangeTextState(false);
+            }
+        }
+        else
+        {
+            if (lastHitGO != null)
+                lastHitGO.transform.parent.GetComponent<DisplayText>().ChangeTextState(false);
+        }
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-
             if (Physics.Raycast(ray, out hit, interactDistance))
             {
                 if (hit.collider.CompareTag("Door")) //Door Mesh has to have the tag, because it has the collider
