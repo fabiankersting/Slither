35063fc220b47a993411e2016ecff0ef7fd75469
SirMikelson <81696994+SirMikelson@users.noreply.github.com>
2021-05-31 17:47:34 +0200

    Blitz + Gewitter

---
+++ Assets/Scripts/BlitzSpawnScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class BlitzSpawnScript : MonoBehaviour
+{
+    public GameObject objectToActivate;
+    float randomfloat;
+    public AudioClip thunder;
+    AudioSource audioSource;
+
+   void Start()
+   {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(Deactivate());
+   }
+
+    private IEnumerator Deactivate()
+    {
+        randomfloat = Random.Range(25, 40);
+        yield return new WaitForSeconds(randomfloat);
+
+        audioSource.PlayOneShot(thunder);
+        objectToActivate.SetActive(true);
+
+        yield return new WaitForSeconds(7);
+
+        objectToActivate.SetActive(false);
+
+        yield return Deactivate();
+  
+    }
+}
